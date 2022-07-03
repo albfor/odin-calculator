1c5510ea34bbd6327b5b929d351d02be20bf3ba5
Albin Forsberg <96afo02@gmail.com>
2022-07-03 02:37:56 +0200

    added operate

--- app.js
+++ app.js
@@ -14,9 +14,24 @@ function divide(a, b) {
 	return a / b;
 }
 
+function operate(operation, a, b) {
+	return operation(a, b);
+	// switch (operation) {
+	// 	case "+":
+	// 		return add(a, b);
+	// 	case "-":
+	// 		return subtract(a, b);
+	// 	case "*":
+	// 		return multiply(a, b);
+	// 	case "/":
+	// 		return divide(a, b);
+	// }
+}
+
 module.exports = {
 	add,
 	subtract,
 	multiply,
-	divide
-}
+	divide,
+	operate,
+};
--- app.spec.js
+++ app.spec.js
@@ -16,4 +16,20 @@ describe("app", () => {
 	test("divide 10 by 2", () => {
 		expect(app.divide(10, 2)).toBe(5);
 	});
+
+	test("operate add", () => {
+		expect(app.operate(app.add, 3, 4)).toBe(7);
+	});
+
+	test("operate subtract", () => {
+		expect(app.operate(app.subtract, 3, 4)).toBe(-1);
+	});
+
+	test("operate multiply", () => {
+		expect(app.operate(app.multiply, 3, 4)).toBe(12);
+	});
+
+	test("operate divide", () => {
+		expect(app.operate(app.divide, 3, 4)).toBe(3 / 4);
+	});
 });
