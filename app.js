1ee6f72cff425fbe2f0233892fceddac41d117e9
Albin Forsberg <96afo02@gmail.com>
2022-07-03 17:27:16 +0200

    buttons implemented

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let operator = '';
 let a, b = 0;
+const display = document.getElementById('display');
 
 init();
 
@@ -20,6 +21,8 @@ function divide(a, b) {
 }
 
 function operate(operation, a, b) {
+	a = Number(a);
+	b = Number(b);
 	switch (operation) {
 		case "+":
 			return add(a, b);
@@ -33,21 +36,33 @@ function operate(operation, a, b) {
 }
 
 function init() {
+	display.value = 0;
 	const operations = document.querySelectorAll('.operator');
 	operations.forEach(operator => {
 		operator.addEventListener('click', setOperation);
 	});
 
 	document.getElementById('execute').addEventListener('click', () => {
-		b = document.getElementById('display').value;
-		document.getElementById('display').value = operate(operator, a, b);
+		b = display.value;
+		display.value = operate(operator, a, b);
 	});
 
+	const numbers = document.querySelectorAll('.number');
+	numbers.forEach(n => {
+		n.addEventListener('click', (event) => {
+			if (display.value === '0') {
+				display.value = event.target.textContent;
+			} else {
+				display.value += event.target.textContent;
+			}
+		});
+	});
 }
 
 function setOperation() {
 	operator = this.textContent;
-	a = document.getElementById('display').value;
+	a = display.value;
+	display.value = 0;
 }
 
 
